d9e8fe081c2c4b4d15608c36190be0c8d6ec25c6
Moinul Islam <moinulilm10@gmail.com>
2024-02-19 02:12:39 +0600

    scroll feature added

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
-function toggleMenu() {
-  var menu = document.querySelector(".responsive-menu");
-  menu.classList.toggle("show-menu");
+// Get the button element
+var upButton = document.getElementById("upBtn");
+window.onscroll = function () {
+  scrollFunction();
+};
+
+// Function to handle scroll event
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 300 ||
+    document.documentElement.scrollTop > 300
+  ) {
+    upButton.style.display = "block";
+  } else {
+    upButton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
+
+// for downside of the webpage
+var downArrow = document.getElementById("down-arrow");
+downArrow.addEventListener("click", bottomFunction);
+
+function bottomFunction() {
+  window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
+  document.documentElement.scrollTo({
+    top: document.body.scrollHeight,
+    behavior: "smooth",
+  });
 }
